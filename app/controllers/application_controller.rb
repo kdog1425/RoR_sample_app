ba13d01a8574d441984bafaff175c9fa113e8b58
Khen Price <kenp332@gmail.com>
2015-03-30 13:12:12 -0400

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hello
-  	render text: "hello sample app!"
+  def help
+  end
+
+  def home
+  end
+
+  def about
   end
 end
