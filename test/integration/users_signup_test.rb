6917b4c8093e7cc3753a561c2eb17adbf2e0742d
Khen Price <kenp332@gmail.com>
2015-04-03 13:41:33 -0400

    added css tests, gravatar size, added conten_tag instead of html+erb

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,6 +9,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#<error_explanation>'
+    assert_select 'div.<alert alert-danger>'
   end
 
   test "valid signup information" do
@@ -21,5 +23,5 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/show'
   end
-  
+
 end
