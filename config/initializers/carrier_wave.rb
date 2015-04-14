801f886950c5a52e9674a98499cef863cfb1c25a
Khen Price <kenp332@gmail.com>
2015-04-14 11:27:10 -0400

    config changes

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJSQSFTHLAUZQ$GAQ'],
-      :aws_secret_access_key => ENV['Ye9+iSxT24dMUulwL4GqhCc7n+ZGJcTJ71yHFAmp']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['rubytot-images']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
-
