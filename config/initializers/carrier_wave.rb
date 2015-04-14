13b639eb78ab1233c5b05ebff0f74867af929f42
Khen Price <kenp332@gmail.com>
2015-04-14 11:06:09 -0400

    Added user microposts

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AKIAJSQSFTHLAUZQ$GAQ'],
+      :aws_secret_access_key => ENV['Ye9+iSxT24dMUulwL4GqhCc7n+ZGJcTJ71yHFAmp']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
+end
+
